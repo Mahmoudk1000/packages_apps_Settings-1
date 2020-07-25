Havoc-OS/android_packages_apps_Settings

Settings: Battery temp: Use MCC to determine the scale type
The whole tapping on the preference was cute but really not ideal. Most
users are one or the other and is usually based on where the hell they're
at. Using the MCC does just that.

Change-Id: I048cda9032a817035c32087cfd9ffeb49003d9d1

src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -391,7 +391,9 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         mScreenUsagePref.setSummary(StringUtil.formatElapsedTime(getContext(),
                 mBatteryUtils.calculateScreenUsageTime(mStatsHelper), false));
         mBatteryTemp.setSummary(
-                com.android.internal.util.havoc.Utils.batteryTemperature(getContext(), false));
+                com.android.internal.util.havoc.Utils.mccCheck(getContext()) ?
+                com.android.internal.util.havoc.Utils.batteryTemperature(getContext(), true) + "°F" :
+                com.android.internal.util.havoc.Utils.batteryTemperature(getContext(), false) + "°C");
     }
 
     @VisibleForTesting

Mahmoudk1000/packages_apps_Settings-1

Signed-off-by: ROXAANE <bajithottyadvaith99@gmail.com>

src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -391,7 +391,9 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         mScreenUsagePref.setSummary(StringUtil.formatElapsedTime(getContext(),
                 mBatteryUtils.calculateScreenUsageTime(mStatsHelper), false));
         mBatteryTemp.setSummary(
-                com.android.internal.util.elegant.Utils.batteryTemperature(getContext(), false));
+                com.android.internal.util.elegant.Utils.mccCheck(getContext()) ?
+                com.android.internal.util.elegant.Utils.batteryTemperature(getContext(), true) + "°F" :
+                com.android.internal.util.elegant.Utils.batteryTemperature(getContext(), false) + "°C");
     }
 
     @VisibleForTesting