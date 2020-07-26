Havoc-OS/android_packages_apps_Settings

Settings: Use SwitchPreference from VendorSupport

src/com/android/settings/development/ResizableActivityPreferenceController.java
@@ -21,10 +21,11 @@ import android.provider.Settings;
 
 import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
-import androidx.preference.SwitchPreference;
 
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.development.DeveloperOptionsPreferenceController;
+
+import com.havoc.support.preferences.SwitchPreference;
 
 public class ResizableActivityPreferenceController extends DeveloperOptionsPreferenceController
         implements Preference.OnPreferenceChangeListener, PreferenceControllerMixin {

src/com/android/settings/development/ShowLayoutBoundsPreferenceController.java
@@ -20,11 +20,12 @@ import android.content.Context;
 import android.sysprop.DisplayProperties;
 
 import androidx.preference.Preference;
-import androidx.preference.SwitchPreference;
 
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.development.DeveloperOptionsPreferenceController;
 import com.android.settingslib.development.SystemPropPoker;
+
+import com.havoc.support.preferences.SwitchPreference;
 
 public class ShowLayoutBoundsPreferenceController extends DeveloperOptionsPreferenceController
         implements Preference.OnPreferenceChangeListener, PreferenceControllerMixin {

src/com/android/settings/fuelgauge/AutoRestrictionPreferenceController.java
@@ -19,10 +19,11 @@ import android.content.Context;
 import android.provider.Settings;
 
 import androidx.preference.Preference;
-import androidx.preference.SwitchPreference;
 
 import com.android.settings.core.BasePreferenceController;
 import com.android.settings.overlay.FeatureFactory;
+
+import com.havoc.support.preferences.SwitchPreference;
 
 /**
  * Controller to change and update the auto restriction toggle

src/com/android/settings/network/telephony/DataDuringCallsPreferenceController.java
@@ -28,9 +28,10 @@ import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.OnLifecycleEvent;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.SwitchPreference;
 
 import com.android.settings.network.SubscriptionsChangeListener;
+
+import com.havoc.support.preferences.SwitchPreference;
 
 public class DataDuringCallsPreferenceController extends TelephonyTogglePreferenceController
         implements LifecycleObserver,

src/com/android/settings/security/ScreenPinningSettings.java
@@ -31,7 +31,6 @@ import android.widget.Switch;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.SwitchPreference;
 
 import com.android.internal.widget.LockPatternUtils;
 import com.android.settings.R;
@@ -41,6 +40,8 @@ import com.android.settings.password.ChooseLockGeneric;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.widget.SwitchBar;
 import com.android.settingslib.search.SearchIndexable;
+
+import com.havoc.support.preferences.SwitchPreference;
 
 import java.util.Arrays;
 import java.util.List;

src/com/android/settings/users/UserDetailsSettings.java
@@ -26,11 +26,12 @@ import android.os.UserHandle;
 import android.os.UserManager;
 
 import androidx.preference.Preference;
-import androidx.preference.SwitchPreference;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.RestrictedLockUtilsInternal;
+
+import com.havoc.support.preferences.SwitchPreference;
 
 import java.util.List;
 

src/com/android/settings/utils/ManagedServiceSettings.java
@@ -36,7 +36,6 @@ import android.view.View;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.SwitchPreference;
 
 import com.android.settings.R;
 import com.android.settings.Utils;
@@ -44,6 +43,8 @@ import com.android.settings.core.instrumentation.InstrumentedDialogFragment;
 import com.android.settings.widget.AppSwitchPreference;
 import com.android.settings.widget.EmptyTextSettings;
 import com.android.settingslib.applications.ServiceListing;
+
+import com.havoc.support.preferences.SwitchPreference;
 
 import java.util.List;
 

src/com/android/settings/wifi/WifiWakeupPreferenceController.java
@@ -31,7 +31,6 @@ import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.Fragment;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.SwitchPreference;
 
 import com.android.settings.R;
 import com.android.settings.core.PreferenceControllerMixin;
@@ -42,6 +41,8 @@ import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnPause;
 import com.android.settingslib.core.lifecycle.events.OnResume;
+
+import com.havoc.support.preferences.SwitchPreference;
 
 /**
  * {@link PreferenceControllerMixin} that controls whether the Wi-Fi Wakeup feature should be

Mahmoudk1000/packages_apps_Settings-1

Signed-off-by: ROXAANE <bajithottyadvaith99@gmail.com>
Change-Id: I97a543c2380ee765a2f6938842917c79aba763a5

src/com/android/settings/development/ResizableActivityPreferenceController.java
@@ -21,10 +21,11 @@ import android.provider.Settings;
 
 import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
-import androidx.preference.SwitchPreference;
 
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.development.DeveloperOptionsPreferenceController;
+
+import com.elegant.support.preferences.SwitchPreference;
 
 public class ResizableActivityPreferenceController extends DeveloperOptionsPreferenceController
         implements Preference.OnPreferenceChangeListener, PreferenceControllerMixin {

src/com/android/settings/development/ShowLayoutBoundsPreferenceController.java
@@ -20,11 +20,12 @@ import android.content.Context;
 import android.sysprop.DisplayProperties;
 
 import androidx.preference.Preference;
-import androidx.preference.SwitchPreference;
 
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.development.DeveloperOptionsPreferenceController;
 import com.android.settingslib.development.SystemPropPoker;
+
+import com.elegant.support.preferences.SwitchPreference;
 
 public class ShowLayoutBoundsPreferenceController extends DeveloperOptionsPreferenceController
         implements Preference.OnPreferenceChangeListener, PreferenceControllerMixin {

src/com/android/settings/fuelgauge/AutoRestrictionPreferenceController.java
@@ -19,10 +19,11 @@ import android.content.Context;
 import android.provider.Settings;
 
 import androidx.preference.Preference;
-import androidx.preference.SwitchPreference;
 
 import com.android.settings.core.BasePreferenceController;
 import com.android.settings.overlay.FeatureFactory;
+
+import com.elegant.support.preferences.SwitchPreference;
 
 /**
  * Controller to change and update the auto restriction toggle

src/com/android/settings/network/telephony/DataDuringCallsPreferenceController.java
@@ -28,9 +28,10 @@ import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.OnLifecycleEvent;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.SwitchPreference;
 
 import com.android.settings.network.SubscriptionsChangeListener;
+
+import com.elegant.support.preferences.SwitchPreference;
 
 public class DataDuringCallsPreferenceController extends TelephonyTogglePreferenceController
         implements LifecycleObserver,

src/com/android/settings/security/ScreenPinningSettings.java
@@ -31,7 +31,6 @@ import android.widget.Switch;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.SwitchPreference;
 
 import com.android.internal.widget.LockPatternUtils;
 import com.android.settings.R;
@@ -41,6 +40,8 @@ import com.android.settings.password.ChooseLockGeneric;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.widget.SwitchBar;
 import com.android.settingslib.search.SearchIndexable;
+
+import com.elegant.support.preferences.SwitchPreference;
 
 import java.util.Arrays;
 import java.util.List;

src/com/android/settings/users/UserDetailsSettings.java
@@ -26,11 +26,12 @@ import android.os.UserHandle;
 import android.os.UserManager;
 
 import androidx.preference.Preference;
-import androidx.preference.SwitchPreference;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.RestrictedLockUtilsInternal;
+
+import com.elegant.support.preferences.SwitchPreference;
 
 import java.util.List;
 

src/com/android/settings/utils/ManagedServiceSettings.java
@@ -36,7 +36,6 @@ import android.view.View;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.SwitchPreference;
 
 import com.android.settings.R;
 import com.android.settings.Utils;
@@ -44,6 +43,8 @@ import com.android.settings.core.instrumentation.InstrumentedDialogFragment;
 import com.android.settings.widget.AppSwitchPreference;
 import com.android.settings.widget.EmptyTextSettings;
 import com.android.settingslib.applications.ServiceListing;
+
+import com.elegant.support.preferences.SwitchPreference;
 
 import java.util.List;
 

src/com/android/settings/wifi/WifiWakeupPreferenceController.java
@@ -31,7 +31,6 @@ import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.Fragment;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.SwitchPreference;
 
 import com.android.settings.R;
 import com.android.settings.core.PreferenceControllerMixin;
@@ -42,6 +41,8 @@ import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnPause;
 import com.android.settingslib.core.lifecycle.events.OnResume;
+
+import com.elegant.support.preferences.SwitchPreference;
 
 /**
  * {@link PreferenceControllerMixin} that controls whether the Wi-Fi Wakeup feature should be